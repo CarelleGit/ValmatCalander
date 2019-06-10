b8e1a9fcec54fbebacc1277f459e85ad1b07c9aa
Carelle <steamtik@gmail.com>
2019-06-10 12:32:29 -0700

    Lerped a panel, made particles

---
+++ Assets/Scripts/PanelLerp.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelLerp : MonoBehaviour
+{
+    [SerializeField]
+
+    public GameObject startPoint;
+    public GameObject endPoint;
+
+    public float timer;
+
+    public GameObject panel;
+    [Range(0,1)]
+    public float lerpSpeed;
+  
+    public void PanelSlide()
+    {
+        if(lerpSpeed >=1)
+        {
+            StartCoroutine(ClosePanel());
+           
+        }
+        if(lerpSpeed <= 0)
+        {
+            StartCoroutine(openPanel());
+            
+        }
+    }
+
+    IEnumerator ClosePanel()
+    {
+        while (lerpSpeed != 0)
+        {
+            lerpSpeed -= Time.deltaTime;
+            panel.transform.position = Vector3.Lerp(startPoint.transform.position, endPoint.transform.position, lerpSpeed);    
+            yield return new WaitForSeconds(timer);
+            if(lerpSpeed  <= 0)
+            {
+                lerpSpeed = 0;
+            }
+        }
+
+    }
+    IEnumerator openPanel()
+    {
+        while(lerpSpeed != 1)
+        {
+            lerpSpeed += Time.deltaTime;
+            panel.transform.position = Vector3.Lerp(startPoint.transform.position, endPoint.transform.position, lerpSpeed);
+            yield return new WaitForSeconds(timer);
+            if(lerpSpeed >= 1)
+            {
+                lerpSpeed = 1;
+            }
+        }
+       
+    }
+}
